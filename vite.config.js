7d445d8aee31b833f88c7effb443e80f6429fec3
Henrik <Henrikmattsson89@gmail.com>
2024-12-18 16:03:16 +0100

    adding files

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/kino-bio-projekt/', // Replace 'my-vite-app' with your repository name
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        aboutUs: './about-us.html',
+      },
+    },
+  },
+});
