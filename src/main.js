c9be5d03a8caaaf7c6101cebce6bc1b7e7404f1f
Alfred <alfred-brannarer@hotmail.com>
2024-12-20 12:24:15 +0100

    Imported MovieGenerator and called the CardGenerator function

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 // vite will now compile our scss
 import './styles/styles.scss';
+//JS Import
+import MovieCardGenerator from './js/_frontpage_movie_cards.js';
+
+const movieCardGenerator = new MovieCardGenerator();
+movieCardGenerator.CardGenerator(4);
+
