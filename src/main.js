8e1e1a13ca54310c251009e4a4fee520fce4fc87
Patrik <pat_p3@hotmail.com>
2024-12-29 11:23:15 +0100

    Imports and listener

--- src/main.js
+++ src/main.js
@@ -4,20 +4,25 @@ import './styles/styles.scss';
 import MovieCardGenerator from './js/_frontpage_movie_cards.js';
 import LoadAllFilmsPage from './js/LoadAllFilmsPage.js';
 import ApiBackend from './js/ApiBackend.js';
+import LoadLiveEvents from './js/_frontpage_load_live_events.js';
+import LiveEventDOMHandler from './js/_live_event_DOM_handler.js';
+import initLiveEvents from './js/_initLiveEvents.js';
 
-if(document.querySelector('.movies__list-all')) {
-const loadingMessage = document.createElement('div');
-loadingMessage.classList.add('loading-message');
-loadingMessage.innerText = 'Api is starting\nLoading movies... Please wait.';
-document.querySelector('.loading-message').appendChild(loadingMessage);
-loadingMessage.style.display = 'none';
+if (document.querySelector('.movies__list-all')) {
+  const loadingMessage = document.createElement('div');
+  loadingMessage.classList.add('loading-message');
+  loadingMessage.innerText = 'Api is starting\nLoading movies... Please wait.';
+  document.querySelector('.loading-message').appendChild(loadingMessage);
+  loadingMessage.style.display = 'none';
 
-const backend = new ApiBackend('https://kino-bio-projekt.onrender.com');
-console.log('Link to API:' + backend);
-const filmList = new LoadAllFilmsPage(backend);
+  const backend = new ApiBackend('https://kino-bio-projekt.onrender.com');
+  console.log('Link to API:' + backend);
+  const filmList = new LoadAllFilmsPage(backend);
 
-filmList.add(document.querySelector('.movies__list-all'), loadingMessage);
-} else{
-const movieCardGenerator = new MovieCardGenerator();
-movieCardGenerator.CardGenerator(4);
+  filmList.add(document.querySelector('.movies__list-all'), loadingMessage);
+} else {
+  const movieCardGenerator = new MovieCardGenerator();
+  movieCardGenerator.CardGenerator(4);
 }
+
+document.addEventListener('DOMContentLoaded', initLiveEvents);
