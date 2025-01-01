d8c864c5f109ddfd806a6375780d3562ff8b5d49
Kirill9m <127823714+Kirill9m@users.noreply.github.com>
2025-01-01 20:44:30 +0000

    new class and funktion start forr film rendering with filter

--- src/main.js
+++ src/main.js
@@ -5,18 +5,21 @@ import MovieCardGenerator from './js/_frontpage_movie_cards.js';
 import LoadAllFilmsPage from './js/LoadAllFilmsPage.js';
 import ApiBackend from './js/ApiBackend.js';
 
-if(document.querySelector('.movies__list-all')) {
-const loadingMessage = document.createElement('div');
-loadingMessage.classList.add('loading-message');
+if(document.querySelector('.movies')) {
+const loadingMessage = document.createElement('h4');
+loadingMessage.classList.add('movies__message__new');
 loadingMessage.innerText = 'Api is starting\nLoading movies... Please wait.';
-document.querySelector('.loading-message').appendChild(loadingMessage);
+document.querySelector('.movies__message').appendChild(loadingMessage);
 loadingMessage.style.display = 'none';
 
 const backend = new ApiBackend('https://kino-bio-projekt.onrender.com');
 console.log('Link to API:' + backend);
 const filmList = new LoadAllFilmsPage(backend);
+const moviesContainer = document.querySelector('.movies')
 
-filmList.add(document.querySelector('.movies__list-all'), loadingMessage);
+filmList.start(moviesContainer, loadingMessage);
+
+
 } else{
 const movieCardGenerator = new MovieCardGenerator();
 movieCardGenerator.CardGenerator(4);
