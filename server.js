7cbe1cca5c119ba2ea0447436b150221dadbd0b6
Alfred <alfred-brannarer@hotmail.com>
2025-01-14 14:03:57 +0100

    Updated server.js so it works with Handlebars

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 import renderHeader from './src/lib/renderPage.js';
+import path from 'path';
 
 const app = express();
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './templates');
 
+
+
 app.get('/', (request, response) => {
-    response.redirect('/kino-bio-projekt');
+    renderHeader(response, 'index');
+});
+
+app.get('/about', (request, response) => {
+    renderHeader(response, 'about-us');
+});
+
+app.get('/movies', (request, response) => {
+    renderHeader(response, 'movies');
 });
 
 app.use('/kino-bio-projekt', express.static('./dist'));
-
-app.get('/kino-bio-projekt', async (request, response) => {
-    const buf = await fs.readFile('./dist/index.html');
-    const html = buf.toString();
-    response.send(html);
-});
+app.use('/kino-bio-projekt/assets', express.static('./dist/assets'));
 
 app.listen(3080, () => {
     console.log('Server running on http://localhost:3080');
