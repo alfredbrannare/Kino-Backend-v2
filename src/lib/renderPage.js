7f8cb453bacb4f026f70d7c1a1e072c2cfbc14d9
Alfred <alfred-brannarer@hotmail.com>
2025-01-14 19:10:07 +0100

    Config for handlebar footer

--- src/lib/renderPage.js
+++ src/lib/renderPage.js
@@ -28,40 +28,25 @@ const headerMenu = [
     },
 ];
 
-const footerMenu = [
-    {
-        label: '',
-        id: '',
-        link: '#',
-    },
-    {
+const footerSection1 = [
+    { label: 'OM KINO', link: 'about' },
+    { label: 'FRÅGOR SVAR', link: '#' },
+    { label: 'KONTAKTA OSS', link: '#' },
+];
 
-    },
-    {
+const footerSection2 = [
+    { label: 'PRESENTKORT', link: '#' },
+    { label: 'TILLGÄNGLIGHET', link: '#' },
+    { label: 'MEDLEMSKAP', link: '#' },
+    { label: 'BISTRO-BIO', link: '#' },
+    { label: 'EVENEMANG', link: '#' },
+];
 
-    },
-    {
+const footerSection3 = [
+    { label: 'FACEBOOK', link: '#' },
+    { label: 'INSTAGRAM', link: '#' },
+];
 
-    },
-    {
-
-    },
-    {
-
-    },
-    {
-
-    },
-    {
-
-    },
-    {
-
-    },
-    {
-
-    },
-]
 
 export default async function renderHeader(response, page) {
     response.render(page, {
@@ -71,6 +56,8 @@ export default async function renderHeader(response, page) {
                 link: item.link,
             };
         }),
-        footerItems: footerMenu,
+        footerSection1,
+        footerSection2,
+        footerSection3,
     });
 }
