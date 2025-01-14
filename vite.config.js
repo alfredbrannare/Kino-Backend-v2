64f4f184244161305abf122ed2184655463241b0
Alfred <alfred-brannarer@hotmail.com>
2025-01-14 14:04:13 +0100

    Updates for Handlebars compatibillity

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
+import path, { dirname } from 'path';
 
 export default defineConfig({
   base: '/kino-bio-projekt/', // Replace 'my-vite-app' with your repository name
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
-        main: './index.html',
-        aboutUs: './about-us.html',
-        movies: './movies.html',
+        main: path.resolve(__dirname, 'src/main.js'),
       },
     },
   },
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname, '/templates'),
+      partialDirectory: path.resolve(__dirname, 'templates'),
       context: {
         siteTitle: 'Kino Bio Projekt',
         footerText: '© 2025 Kino Bio',
-      }
+      },
     })
   ],
 });
