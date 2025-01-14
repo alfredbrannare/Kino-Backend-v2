91bb03f52a5bca5ed1548b8d7d20d151041c05ea
Alfred <alfred-brannarer@hotmail.com>
2025-01-14 14:04:43 +0100

    Implemented Handlebars

--- src/lib/renderPage.js
+++ src/lib/renderPage.js
@@ -1,44 +1,76 @@
 import fs from 'fs/promises'
 
-const headerMenu = [{
-    label:  'Home page',
-    id: 'index',
-    link: '/kino-bio-projekt/index.html',
-},
-{
-    label:  'BILJETTER',
-    id: 'biljetter',
-    link: '#',
-},
-{
-    label:  'EVENEMANG',
-    id: 'evenemang',
-    link: '#',
-},
-{
-    label:  'FILMER',
-    id: 'filmer',
-    link: '/movies.html',
-},
-{
-    label:  'MEDLEMSKAP',
-    id: 'medlemskap',
-    link: '#',
-},
-{
-    label:  'OM OSS',
-    id: 'omoss',
-    link: '/about-us.html',
-},
+const headerMenu = [
+    {
+        label: 'BILJETTER',
+        id: 'biljetter',
+        link: '#',
+    },
+    {
+        label: 'EVENEMANG',
+        id: 'evenemang',
+        link: '#',
+    },
+    {
+        label: 'FILMER',
+        id: 'filmer',
+        link: 'movies',
+    },
+    {
+        label: 'MEDLEMSKAP',
+        id: 'medlemskap',
+        link: '#',
+    },
+    {
+        label: 'OM OSS',
+        id: 'omoss',
+        link: 'about-us',
+    },
 ];
 
+const footerMenu = [
+    {
+        label: '',
+        id: '',
+        link: '#',
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+]
+
 export default async function renderHeader(response, page) {
-    response.render({
+    response.render(page, {
         headerItems: headerMenu.map((item) => {
             return {
                 label: item.label,
                 link: item.link,
             };
-        })
+        }),
+        footerItems: footerMenu,
     });
 }
